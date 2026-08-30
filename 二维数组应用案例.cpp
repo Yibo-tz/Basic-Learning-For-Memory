19787a766ddb44f6d8ecb1dd5a0cc1470441ee7a
Paul Zhu <Zj20071126yb@163.com>
2026-08-30 17:33:44 +0800

    完善了二维数组的数组名，同时还增加了实际应用的案例

---
+++ 二维数组应用案例.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+int main() {
+	//1、创建二维数组
+	int scores[3][3] = {
+		{100,100,100},
+		{90,50,100},
+		{60,70,80}
+	};
+
+	//2、统计每个人的总和分数
+	for (int i = 0; i < 3; i++) {
+		 
+	}
+	system("pause");
+	return 0;
+}
---
+++ 二维数组的数组名.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+int main() {
+	//二维数组的名称用途
+	
+	//1.查看二维数组所占内存空间
+	int arr[2][3] = {
+		{1,2,3},
+		{4,5,6}
+	};
+	std::cout << "二维数组占用的内存空间为：" << sizeof arr <<"字节"<< std::endl;
+	
+	//2.获取二维数组的首地址
+	double arr1[2][3] = {
+		{1,2,3},
+		{4,5,6}
+	};
+	std::cout << "大伯版二维数组的占用空间为：" << sizeof(arr1) <<"字节"<< std::endl;
+	std::cout << "大伯版二维数组中第一行占用内存为：" << sizeof(arr1[0]) <<"字节" << std::endl;
+	std::cout << "二维数组第一个元素占用内存为：" << sizeof(arr[0][0]) <<"字节"<< std::endl;
+
+	std::cout << "二维数组的行数为：" << sizeof(arr) / sizeof(arr[0])<<"\n";
+	std::cout << "二维数组的列数为：" << sizeof(arr[0]) / sizeof(arr[0][0]) << std::endl;
+	std::cout << "二维数组的第一行首地址为：" << (int)arr[0] << std::endl;
+	std::cout << "二维数组的第二行首地址为：" << (int)&arr[1] << std::endl;
+
+	std::cout << "二维数组第一个元素的首地址为：" << (int)&arr[0][0] << std::endl;
+	system("pause");
+	return 0;
+}
