602ed266b1e21f177564e9662cae44ab467d2110
Paul Zhu <Zj20071126yb@163.com>
2026-09-08 20:37:23 +0800

    完善了二维数组的应用案例

--- 二维数组应用案例.cpp
+++ 二维数组应用案例.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+﻿#include<iostream>
 int main() {
 	//1、创建二维数组
 	int scores[3][3] = {
@@ -6,10 +6,14 @@ int main() {
 		{90,50,100},
 		{60,70,80}
 	};
-
+	std::string names[3] = { "张三","李四","王五" };
 	//2、统计每个人的总和分数
 	for (int i = 0; i < 3; i++) {
-		 
+		int sum = 0;//统计分数总和
+		for (int j = 0; j < 3; j++) {
+			sum += scores[i][j];
+		 }
+		std::cout << names[i]<< "的总分为：" << sum << std::endl;
 	}
 	system("pause");
 	return 0;
