a8e945c86034b4938c14bf959343f98ee561e370
Paul Zhu <Zj20071126yb@163.com>
2026-09-16 21:36:05 +0800

    加了更多结构体添加方式

--- 结构体.cpp
+++ 结构体.cpp
@@ -10,6 +10,7 @@
 
 //创建一个学生的数据类型：学生包括（姓名，年龄，分数）
 //自定义的数据类型，一些类型集合组成了一个类型
+
 struct student {
 	//成员列表
 	//姓名
@@ -18,9 +19,10 @@ struct student {
 	int age = 0;
 	//考试分数
 	int score = 0;
-};
+}s3;//写这里基本上很难看到，为了代码的可读性建议不要选这个方法
 //2、通过学生类型创建具体学生
 int main() {
+	//struct的关键字可以省略，但是定义时不可以省略（或许仅限C++？） 
 	struct student s1;
 	//给s1属性赋值，通过.访问结构体变量中的属性
 	s1.name = "张三";
@@ -31,4 +33,10 @@ int main() {
 	//struct student s2={……}
 	struct student s2 = { "李四",19,80 };
 	std::cout << "姓名：" << s2.name << "\t年龄：" << s2.age << "\t分数：" << s2.score << std::endl;
+
+	//在定义结构体时顺便创建结构体变量
+	s3.name = "王五";
+	s3.age = 20;
+	s3.score = 60;
+	std::cout << "姓名：" << s3.name << "\t年龄：" << s3.age << "\t分数：" << s3.score << std::endl;
 }
